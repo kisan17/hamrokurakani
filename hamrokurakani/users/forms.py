482cc74abb75720a65d9506f5b72e8b7ad48210b
Siddharth Belbase <siddharthbelbase@gmail.com>
2021-04-15 10:20:15 +0545

    fix settings form

--- hamrokurakani/users/forms.py
+++ hamrokurakani/users/forms.py
@@ -50,10 +50,6 @@ class UpdateAccountForm(FlaskForm):
     username = StringField('Username', validators=[
                            DataRequired(), Length(min=2, max=15)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    #age = IntegerField('Age', validators=[DataRequired()])
-    #gender = RadioField('Gender',choices=[('F', 'FEMALE') , ('M','MALE')], validators=[DataRequired("Please Select Your Gender!")])
-    bio = StringField('Bio', validators=[
-                      DataRequired(), Length(min=0, max=100)])
     picture = FileField('Update Profile Picture', validators=[
                         FileAllowed(['png', 'jpg'])])
     submit = SubmitField('Update')
@@ -78,21 +74,3 @@ class UpdateAccountForm(FlaskForm):
             if user:
                 raise ValidationError(
                     'This email has already been registered. Please choose a different one.')
-
-
-class RequestResetForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    submit = SubmitField('Request Password Reset')
-
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user is None:
-            raise ValidationError(
-                'There is no account with that email. You must register first.')
-
-
-class ResetPasswordForm(FlaskForm):
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[
-                                     DataRequired(), EqualTo('password')])
-    submit = SubmitField('Reset Password')
